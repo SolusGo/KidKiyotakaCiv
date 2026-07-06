f720db0357ced66ba6b68c03a0ae3f854443a93a
SolusGo <gabrielcarylat1@gmail.com>
2026-07-06 14:15:05 +1000

    Hide White Room dummy building metadata

--- SQL/WhiteRoomCompatibilityText.sql
+++ SQL/WhiteRoomCompatibilityText.sql
@@ -6,4 +6,24 @@
 INSERT OR IGNORE INTO Language_en_US (Tag, Text)
 VALUES
 ('TXT_KEY_CITYVIEW_HAPPINESS_TEXT', 'Happiness'),
-('TXT_KEY_CITYVIEW_UNHAPPINESS_TEXT', 'Unhappiness');
+('TXT_KEY_CITYVIEW_UNHAPPINESS_TEXT', 'Unhappiness'),
+('TXT_KEY_BUILDING_WR_DUMMY_HIDDEN', '');
+
+-- Keep White Room dummy buildings out of CP/EUI CityView lists. These buildings
+-- are mechanical counters only and should not appear as city buildings,
+-- specialist containers, great-work buildings, or production entries.
+UPDATE BuildingClasses
+SET Description = 'TXT_KEY_BUILDING_WR_DUMMY_HIDDEN'
+WHERE Type LIKE 'BUILDINGCLASS_WR_%';
+
+UPDATE Buildings
+SET Description = 'TXT_KEY_BUILDING_WR_DUMMY_HIDDEN',
+    Civilopedia = 'TXT_KEY_BUILDING_WR_DUMMY_HIDDEN',
+    Strategy = 'TXT_KEY_BUILDING_WR_DUMMY_HIDDEN',
+    Help = 'TXT_KEY_BUILDING_WR_DUMMY_HIDDEN',
+    GreatWorkCount = -1,
+    NeverCapture = 1,
+    NukeImmune = 1,
+    HurryCostModifier = -1,
+    ConquestProb = 0
+WHERE Type LIKE 'BUILDING_WR_%';
